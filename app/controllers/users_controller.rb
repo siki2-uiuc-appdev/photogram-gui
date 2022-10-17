c1c41593dfdd51a45b75f289df09d108d255b0cf
siki2-uiuc <siki2@illinois.edu>
2022-10-17 19:41:17 +0000

    User details looks good

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,4 +7,21 @@ class UsersController < ApplicationController
 
     render({ :template => "user_templates/index.html.erb"})
   end
+
+  def show
+    # Parameters: {"path_usenname"=>"anisa"}
+
+    url_username = params.fetch("path_username")
+
+    matching_usernames = User.where({ :username => url_username })
+
+    @the_user = matching_usernames.first
+
+    # if !the_user
+    #   redirect_to("/404")
+    # else
+    render({ :template => "user_templates/show.html.erb"})
+    # end
+  end
+
 end
