0d3ee2be91044d4f2ae17a7b4a7d20c7af3edd36
siki2-uiuc <siki2@illinois.edu>
2022-10-17 21:05:23 +0000

    photos delete functionality works

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,7 @@ Rails.application.routes.draw do
   get("/users/:path_username", { :controller => 'users', :action => "show"})
 
   get("/photos", { :controller => 'photos', :action => "index"})
+  get("/photos/:path_id", { :controller => 'photos', :action => "show"})
+  get("delete_photo/:toast_id", { :controller => 'photos', :action => "delete"})
 
 end
